16fc5003a46fc4c0f24d2367674c26c39874a807
Silly Inventor <jamesdavis3004@gmail.com>
2016-07-21 13:06:39 -0400

    Removed Coordinate Scaling
    Whoops, that should never have been in cornrows. Damn you copy paste.

--- Cornrow Curves/code.js
+++ Cornrow Curves/code.js
@@ -1,4 +1,3 @@
-var coordinateScale = 18
 SpriteMorph.flippedY = false;
 SpriteMorph.flippedX = false;
 Costume.colored = false;
@@ -174,31 +173,6 @@ Costume.prototype.setColorWithParent = function(col, myParent) {
 	{
 		myParent.flipXAxis();
 	}
-};
-
-IDE_Morph.prototype.updateCorralBar = function () {
-   
-   var MouseX = this.stage.reportMouseX();
-   var MouseY = this.stage.reportMouseY();
-   Morph.prototype.trackChanges = false;
-   if(this.isSmallStage ||
-      MouseX > StageMorph.prototype.dimensions.x / 2 ||
-      MouseY > StageMorph.prototype.dimensions.y / 2 ||
-      MouseX < StageMorph.prototype.dimensions.x / -2 ||
-      MouseY < StageMorph.prototype.dimensions.y / -2) 
-   {
-     this.corralBar.children[2].text = "";
-     this.corralBar.children[3].text = "";     
-   } else {
-     this.corralBar.children[2].text = "X: " + Math.round(this.stage.reportMouseX() / coordinateScale);
-     this.corralBar.children[3].text = "Y: " + Math.round(this.stage.reportMouseY() / coordinateScale);
-   }
-
-   this.corralBar.children[2].drawNew();
-   this.corralBar.children[3].drawNew();
-   Morph.prototype.trackChanges = true;
-   this.corralBar.changed();
-  
 };
 Costume.prototype.copy = function () {
     var canvas = newCanvas(this.extent()),
