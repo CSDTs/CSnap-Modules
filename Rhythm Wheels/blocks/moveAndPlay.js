90da4d51bc72c5d677d07c12ca085717279d5cc0
Silly Inventor <jamesdavis3004@gmail.com>
2016-07-29 16:26:26 -0400

    Rhythm Wheels Initial Setup Mostly Complete
    Still not working...

---
+++ Rhythm Wheels/blocks/moveAndPlay.js
@@ -0,0 +1,26 @@
+(function () {
+    return function (X, Y, startAngle) {
+        var heading = startAngle+((window.audioContext.currentTime-startTime)*360)/(eighthNoteTime*32);
+        var sound = this.costume.name;
+        
+        if (heading<0) // if the start timer has been reset
+        {
+            startAngle = angle%360;
+            var heading = startAngle+((window.audioContext.currentTime-startTime)*360)/(eighthNoteTime*32);
+        }
+        
+		this.goto(X,Y);
+        this.setHeading(heading)
+        this.forward(30);
+        
+        if(heading >= 360 ) //if you have made it to the start position
+        {
+          this.playSoundTime(sound, startTime + time);
+          startAngle -= 360;
+        }
+        
+	};
+}());
+
+
+//# sourceURL=moveAndPlay.js
