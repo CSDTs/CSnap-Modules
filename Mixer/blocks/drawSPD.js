a5fbd7c7d20ad3cf2e652c79eed67743c39bf5fd
bbabbitt2003 <bill@wbabbitt.com>
2016-08-16 22:36:50 -0400

    sinusoidal display

--- Mixer/blocks/drawSPD.js
+++ Mixer/blocks/drawSPD.js
@@ -8,7 +8,7 @@
 		for(i = 0; i<frequencyDataArray.length/2; i++)
 		{
 			this.changeXPosition(step);
-			this.setYPosition(Math.sin(frequencyDataArray[i]-offset));
+			this.setYPosition(Math.sin(2*Math.PI*frequencyDataArray[i]-offset*step));
 		}
 		this.up();
 	};
