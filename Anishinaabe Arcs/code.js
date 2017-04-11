41ffbcc397c15dd6a0a7ed26011e8bbc54057ba6
SillyInventor <SillyInventor@gmail.com>
2017-04-11 17:54:49 -0400

    Improving 3D Adding grid Increasing viewport

--- Anishinaabe Arcs/code.js
+++ Anishinaabe Arcs/code.js
@@ -139,11 +139,135 @@ SpriteMorph.prototype.point3D = function (degX, degY, degZ) {
 
         this.parent.changed();
     }
-}
-function round10(val,exp)
-{
+};
+
+StageMorph.prototype.addCoordinatePlane = function (){
+    var geometry, grid, xaxis, xlabel, yaxis, ylabel, zaxis, zlabel, material, size = 200, step = 20;
+
+    geometry = new THREE.Geometry();
+    material = new THREE.LineBasicMaterial({color: 'black'});
+
+    for ( var i = -size; i <= size; i+= step) {
+        geometry.vertices.push(new THREE.Vector3( -size, -0.04, i ));
+        geometry.vertices.push(new THREE.Vector3( size, -0.04, i ));
+        geometry.vertices.push(new THREE.Vector3( i, -0.04, -size ));
+        geometry.vertices.push(new THREE.Vector3( i, -0.04, size ));
+    }
+    
+    grid = new THREE.Line(geometry, material, THREE.LinePieces);
+    
+    object = new THREE.Object3D();
+    object.add(grid);
+    
+    geometry = new THREE.Geometry();
+    material = new THREE.LineBasicMaterial({linewidth: 5, linecap: 'round', color: 'red'});
+    
+    geometry.vertices.push(new THREE.Vector3( 0, -0.04, 0 ));
+    geometry.vertices.push(new THREE.Vector3( size, -0.04, 0 ));
+    
+    xaxis = new THREE.Line(geometry, material, THREE.LinePieces);
+    
+    object.add(xaxis);
+    
+    var textShapes = THREE.FontUtils.generateShapes( 'X', {size:12});
+    var text = new THREE.ShapeGeometry( textShapes );
+    xlabel = new THREE.Mesh( text, new THREE.MeshBasicMaterial( { color: 'red' } ) ) ;
+    xlabel.position = new THREE.Vector3(size, 0, 0);
+    xlabel.rotation.x = -Math.PI/2;
+    
+    object.add(xlabel);
+    
+    geometry = new THREE.Geometry();
+    material = new THREE.LineBasicMaterial({linewidth: 5, linecap: 'round', color: 'green'});
+    
+    geometry.vertices.push(new THREE.Vector3( 0, -0.04, 0 ));
+    geometry.vertices.push(new THREE.Vector3( 0, -0.04, -size ));
+    
+    yaxis = new THREE.Line(geometry, material, THREE.LinePieces);
+    
+    object.add(yaxis);
+    
+    var textShapes = THREE.FontUtils.generateShapes( 'Y', {size:12});
+    var text = new THREE.ShapeGeometry( textShapes );
+    ylabel = new THREE.Mesh( text, new THREE.MeshBasicMaterial( { color: 'green' } ) ) ;
+    ylabel.position = new THREE.Vector3(0, 0, -size);
+    ylabel.rotation.x = -Math.PI/2;
+    
+    object.add(ylabel);
+    
+    geometry = new THREE.Geometry();
+    material = new THREE.LineBasicMaterial({linewidth: 5, linecap: 'round', color: 'blue'});
+    
+    geometry.vertices.push(new THREE.Vector3( 0, 0, 0 ));
+    geometry.vertices.push(new THREE.Vector3( 0, size, 0 ));
+    
+    zaxis = new THREE.Line(geometry, material, THREE.LinePieces);
+    
+    object.add(zaxis);
+    
+    var textShapes = THREE.FontUtils.generateShapes( 'Z', {size:12});
+    var text = new THREE.ShapeGeometry( textShapes );
+    zlabel = new THREE.Mesh( text, new THREE.MeshBasicMaterial( { color: 'blue' } ) ) ;
+    zlabel.position = new THREE.Vector3(0, size, 0);
+    zlabel.rotation.x = 0;
+    
+    object.add(zlabel);
+    
+    //this.shownObjects.add(object); // erase this object later by the 'clear' block
+    this.scene.add(object);
+    this.changed();
+};
+
+StageMorph.prototype.init = function (globals) {
+    this.name = localize('Stage');
+    this.threads = new ThreadManager();
+    this.variables = new VariableFrame(globals || null, this);
+    this.scripts = new ScriptsMorph(this);
+    this.customBlocks = [];
+    this.globalBlocks = [];
+    this.costumes = new List();
+    this.costume = null;
+    this.sounds = new List();
+    this.version = Date.now(); // for observers
+    this.isFastTracked = false;
+    this.cloneCount = 0;
+    this.volume = 100;
+    this.muted = false;
+
+    this.timerStart = Date.now();
+    this.tempo = 60; // bpm
+    this.lastMessage = '';
+
+    this.watcherUpdateFrequency = 2;
+    this.lastWatcherUpdate = Date.now();
+
+    this.scale = 1; // for display modes, do not persist
+
+    this.keysPressed = {}; // for handling keyboard events, do not persist
+    this.blocksCache = {}; // not to be serialized (!)
+    this.paletteCache = {}; // not to be serialized (!)
+    this.lastAnswer = null; // last user input, do not persist
+    this.activeSounds = []; // do not persist
+
+    this.trailsCanvas = null;
+    this.isThreadSafe = false;
+
+    StageMorph.uber.init.call(this);
+
+    this.acceptsDrops = false;
+    this.setColor(new Color(255, 255, 255));
+    this.fps = this.frameRate;
+
+    // 3D
+    this.canvas3D = null;
+    this.shownObjects = new List();
+    this.hiddenObjects = new List();
+    this.init3D();
+    this.addCoordinatePlane();
+};
+
+function round10(val,exp) {
 	var pow = Math.pow(10,exp);
 	return Math.round(val/pow)*pow;
 }
-
 //# sourceURL=code.js
