008f498f2197913cf47043ec2e5c03df41c0363e
SillyInventor <SillyInventor@gmail.com>
2017-05-10 15:04:52 -0400

    Added code that allows modal dialogs to appear on start.

--- Tutorial/code.js
+++ Tutorial/code.js
@@ -2175,5 +2175,5 @@ window.click += Unscroll;
 window.parent.onload = setScrollPos;
 window.parent.onscroll = setScrollPos;
 window.onmousedown = Unscroll;
-
+window.parent.document.getElementById('openModalBtn').click();
 //# sourceURL=code.js
