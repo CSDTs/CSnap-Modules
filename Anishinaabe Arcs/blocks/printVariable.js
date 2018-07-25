baac0fb1667f4bf74a9ae43b9ccc039467512f7a
holmr <holmr@rpi.edu>
2018-07-25 11:59:47 -0400

    Make the print(list) block save instead of popping open a new window

--- Anishinaabe Arcs/blocks/printVariable.js
+++ Anishinaabe Arcs/blocks/printVariable.js
@@ -1,6 +1,22 @@
 (function () {
     return function (list) {
-            window.open('data:text/'+ 'plain,' + list.asText());
+        function download(filename, text) {
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            element.setAttribute('download', filename);
+            
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            
+            element.click();
+            
+            document.body.removeChild(element);
+        }
+          
+        // Start file download.
+        download("arcs_list.txt", list.asText());
+
+        // window.open('data:text/'+ 'plain,' + list.asText());
     };
 }());
 
