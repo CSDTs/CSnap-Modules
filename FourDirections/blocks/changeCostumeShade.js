87ae3b3f0c7adca8d9bc373900def94308d3c81a
Andrew Hunn <ahunn@umich.edu>
2019-11-15 15:22:16 -0500

    Convert Quilting Projects to Applications

---
+++ FourDirections/blocks/changeCostumeShade.js
@@ -0,0 +1,51 @@
+(function () {
+	return function (num) {
+	num = num*255/100;
+	var flipBackX = false, flipBackY = false, costume = this.costumes.contents[this.getCostumeIdx()-1];
+	if(this.flippedY)
+	{
+		this.flipYAxis();
+		flipBackY = true;
+	}
+	if(this.flippedX)
+	{
+		this.flipXAxis();
+		flipBackX = true;
+	}
+	if(!costume.originalPixels) {
+		costume.originalPixels = costume.contents.getContext('2d')
+		   .getImageData(0, 0, costume.contents.width,
+			  costume.contents.height);
+	 }
+     if(!costume.costumeColor) {
+         costume.costumeColor = new Color(0,0,0);
+     }
+	var temp = costume.contents.getContext('2d')
+		   .getImageData(0, 0, costume.contents.width,
+			  costume.contents.height);
+	 for(var I = 0, L = temp.data.length; I < L; I += 4){
+		if(temp.data[I + 3] > 0){
+		   // If it's not a transparent pixel
+		   temp.data[I] = temp.data[I] / 255 * num;
+		   temp.data[I + 1] = temp.data[I + 1] / 255 * num;
+		   temp.data[I + 2] = temp.data[I + 2] / 255 * num;
+		}
+	 }
+	 costume.contents.getContext('2d')
+		.putImageData(temp, 0, 0);
+	this.costumes.contents[this.getCostumeIdx()-1] = costume;
+	if(flipBackY)
+	{
+		this.flipYAxis();
+	}
+	if(flipBackX)
+	{
+		this.flipXAxis();
+	}
+    this.changed();
+    this.drawNew();
+	};
+}());
+
+
+//# sourceURL=changeCostumeShade.js
---
+++ FourDirections/blocks/flipXAxis.js
@@ -0,0 +1,34 @@
+(function () {
+    return function () {
+		var costume = this.costumes.contents[this.getCostumeIdx()-1],
+		canvas = newCanvas(costume.extent()),
+		ctx = canvas.getContext('2d');
+		
+		ctx.translate(0, costume.height());
+		ctx.scale(1, -1);
+		ctx.drawImage(costume.contents, 0, 0);
+		costume.contents=canvas;
+		costume.rotationCenter = new Point(
+			   costume.rotationCenter.x,
+			   costume.height() - costume.rotationCenter.y
+		);
+		
+		canvas = newCanvas(costume.extent()),
+		ctx = canvas.getContext('2d');
+		ctx.putImageData(costume.originalPixels, 0, 0);
+		ctx.translate(0, costume.originalPixels.height);
+		ctx.scale(1, -1);
+		costume.originalPixels=ctx.getImageData(0,0,costume.originalPixels.width,costume.originalPixels.height);
+		
+		this.costumes.contents[this.getCostumeIdx()-1] = costume;
+		this.costume = costume;
+		this.flippedX = !this.flippedX;
+		this.isNotFlipBack = !this.isNotFlipBack;
+		this.changed();
+		this.drawNew();
+		this.changed();
+        this.positionTalkBubble();
+    };
+}());
+
+//# sourceURL=reflectX.js
