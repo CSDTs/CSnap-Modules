d35bba6123aa59b2515b4aaf89498ece6e784ac9
angsalacata <angsala@umich.edu>
2020-07-20 13:47:03 -0400

    Using better method to round up time to tenths place. Reformatting

--- Rhythm Wheels/blocks/eighthNoteTime.js
+++ Rhythm Wheels/blocks/eighthNoteTime.js
@@ -1,10 +1,6 @@
 (function () {
     return function (sound, time) {
-		console.log(soundBuffer);
-		this.testWithModules();
 		return eighthNoteTime = (60 / this.getTempo()) / 2;;
 	};
 }());
-
-
 //# sourceURL=eighthNoteTime.js
