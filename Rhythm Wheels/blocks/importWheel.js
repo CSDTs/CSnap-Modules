bc883ee86baf78e00979a3539659a23fa6fa7a1c
angsalacata <angsala@umich.edu>
2020-07-27 15:40:47 -0400

    reformatting in new import code

--- Rhythm Wheels/blocks/importWheel.js
+++ Rhythm Wheels/blocks/importWheel.js
@@ -1,24 +1,13 @@
 (function () {
     return function (fileName, indexNumber, wheelName) {
-        console.log("IMPORT WHEEL CALL");
-        console.log(fileName);
         if (!(wheelMap.hasOwnProperty(wheelName))){
             console.log("Wheel not found/not instantiated");
             throw new Error("Wheel not found/not instantiated");
         }
-        // get sound buffer under soundInput name
-        // let secondsPerBeat = (60.0/this.getTempo());
-        // secondsPerBeat *= timeInput;
-
         let wheelBufferStore = [];
         let file = JSON.stringify(fileBuffer[fileName]);
-        console.log(file);
-        // start parsing
-        // pass in the WheelToChange and input the audio into that wheel
-        console.log(parser);
         parser.parse(JSON.parse(file), wheelBufferStore, indexNumber);
 
-        console.log(wheelBufferStore);
         let wheelToChange = wheelMap[wheelName];
         let secondsPerBeat = (60.0/this.getTempo());
         // now iterate through and put audio wheel data structure
@@ -28,9 +17,6 @@
             secondsPerBeat = Math.ceil(secondsPerBeat * 100) / 100;
             wheelToChange.duration += Math.max(secondsPerBeat, audio.duration);
         }
-       console.log(wheelMap[wheelName]);
-
-       
 	};
 }());
 //# sourceURL=importWheel.js
